13f83d828048a71c5e743c9b03f65e9c6c3a64e2
Edmund Miller <edmund.a.miller@protonmail.com>
2023-11-29 13:43:56 -0500

    Add components

---
+++ 2023-11-29/nf-summit-23-boston/components/Multiqc.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="absolute right-30px bottom-30px">
+    <!-- TODO Make sure this Yellow is right -->
+    <simple-icons-duckdb class="text-4xl text-[#fff000]" />
+  </div>
+</template>
---
+++ 2023-11-29/nf-summit-23-boston/components/Nextflow.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="absolute right-30px bottom-30px">
+    <!-- TODO Make sure this Yellow is right -->
+    <div class="h-16 w-16 flex items-center justify-center m-12">
+      <!-- Add margin class here -->
+      <img
+        src="https://github.com/nextflow-io/trademark/raw/master/nextflow-icon.svg"
+        class="h-full w-full object-cover"
+      />
+    </div>
+  </div>
+</template>
