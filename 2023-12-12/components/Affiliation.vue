50dd5a40373888a2822d6f926d7f4ac93ee4d88b
Edmund Miller <edmund.a.miller@protonmail.com>
2023-12-10 22:07:00 -0600

    Cut out some boilerplate

---
+++ 2023-12-12/components/Affiliation.vue
@@ -0,0 +1,14 @@
+<!-- TODO -->
+<template>
+  Your name /
+  <img src="https://openmoji.org/data/color/svg/E040.svg" width="50" />
+  @twitter*handle /
+  <img src="https://openmoji.org/data/color/svg/E045.svg" width="50" />
+  @github_handle \
+  <a href="https://mstdn.science/@nf_core"
+    ><img
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Mastodon_Logotype*%28Simple%29.svg/216px-Mastodon*Logotype*%28Simple%29.svg.png"
+      width="5%"
+  /></a>
+  @mtdn_handle Your affiliation
+</template>
